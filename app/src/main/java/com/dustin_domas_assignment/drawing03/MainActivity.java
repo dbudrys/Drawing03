cc976704951e8269a17289b6555897e86da7cba7
dustinlobato <dlobato85@gmail.com>
2017-03-16 12:10:13 -0500

    Created DrawActivity and the Intent from MainActivity

--- app/src/main/java/com/dustin_domas_assignment/drawing03/MainActivity.java
+++ app/src/main/java/com/dustin_domas_assignment/drawing03/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dustin_domas_assignment.drawing03;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,18 @@ public class MainActivity extends AppCompatActivity {
 
         //assignment 03
         //mic check
+
+        Button play_b = (Button) findViewById(R.id.play_button);
+        play_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        DrawActivity.class); //Explicit intent
+
+                startActivity(intent);
+            }
+        });// end of onclick
+
 
     }
 }
